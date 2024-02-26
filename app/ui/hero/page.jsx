c5952220bcd4dbb448ac7064dc73434d2e666b1f
six-shot <okhuomonajayi54@gmail.com>
2024-02-26 09:13:34 -0800

    hero

--- app/ui/hero/page.jsx
+++ app/ui/hero/page.jsx
@@ -6,9 +6,52 @@ export default function Hero() {
     <div className="bg-white w-full">
       <div className="max-w-[1440px] mx-auto px-[4%] ">
         <Navbar />
-        <div className="sm:h-[35vh] flex items-center ">
-          <div className="grid sm:grid-cols-12 grid-cols-1 w-full">
-            <div className="col-span-9">yy</div>
+        <div className="sm:h-[35vh] pt-[5%]   flex items-center ">
+          <div className="grid sm:grid-cols-12 grid-cols-1 w-full items-center">
+            <div className="col-span-9">
+              <div className="flex flex-col">
+                <div>
+                  <h1 className="sm:text-[65px] text-[40px] leading-[55px] ">
+                    ARCHITECTURAL
+                  </h1>
+                </div>
+                <div className="sm:flex hidden items-center gap-10">
+                  <div className="">
+                    <h4 className="text-right text-xs font-medium">
+                      MODERN
+                      <br />
+                      BEACHFRONT
+                      <br />
+                      VILLAS
+                    </h4>
+                  </div>
+                  <div className="">
+                    <h1 className=" sm:text-[65px] text-[40px] ">
+                      COSTAL DESIGN
+                    </h1>
+                  </div>
+                </div>
+                <div className="sm:hidden flex flex-col">
+                  <div className="flex  items-center gap-5">
+                    <div className="">
+                      <h4 className="text-right text-[10px] font-medium">
+                        MODERN
+                        <br />
+                        BEACHFRONT
+                        <br />
+                        VILLAS
+                      </h4>
+                    </div>
+                    <div>
+                      <h1 className="  text-[40px] ">COSTAL</h1>
+                    </div>
+                  </div>
+                  <div className="sm:pb-0 pb-[4%]">
+                    <h1 className="  text-[40px] ">DESIGN</h1>
+                  </div>
+                </div>
+              </div>
+            </div>
             <div className="col-span-3 w-full">
               <div className="flex flex-col sm:gap-16 gap-7">
                 <h5 className="text-xs">
