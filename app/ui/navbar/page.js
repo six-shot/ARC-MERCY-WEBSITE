602ccf9660330dd2c223f100026ed03046867f72
six-shot <okhuomonajayi54@gmail.com>
2024-02-26 08:19:08 -0800

    navbar

--- app/ui/navbar/page.js
+++ app/ui/navbar/page.js
@@ -10,6 +10,14 @@ export default function Navbar() {
       <div className="max-w-[1440px] mx-auto px-[4%] ">
         <div className="w-full h-[10vh] border-b-[1px] border-black flex justify-between items-center">
           <div className="flex items-center gap-2">
+            <div className="">
+              <div className="w-[17px] h-[17px] bg-black relative">
+                {" "}
+                <div className="w-[6px] h-[6px] bg-white absolute bottom-[2px] left-[2px]"></div>
+                <div className="w-[6px] h-[6px] bg-white absolute top-[2px] right-[2px]"></div>
+              </div>
+            </div>
+
             <h4 className="font-semibold text-[22px]">MERCY</h4>
             <h4 className="text-[10px] leading-[11px] font-semibold">
               COSTAL <br />
