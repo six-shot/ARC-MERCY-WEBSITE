504ac6643756acecac00a4e5e3dd20ed4835861e
six-shot <okhuomonajayi54@gmail.com>
2024-02-26 08:05:50 -0800

    navbar

--- app/ui/navbar/page.js
+++ app/ui/navbar/page.js
@@ -2,6 +2,8 @@
 import React from "react";
 import { IconContext } from "react-icons";
 import { IoIosMenu } from "react-icons/io";
+import { MdOutlineLocationOn } from "react-icons/md";
+import { RiSearchLine } from "react-icons/ri";
 export default function Navbar() {
   return (
     <header className="bg-white w-full">
@@ -14,12 +16,20 @@ export default function Navbar() {
               ARCHITECTURE
             </h4>
           </div>
-          <div className="flex items-center gap-2">
-            <h4 className="font-medium text-base">MENU</h4>
-            <div className="w-[32px] h-[32px] flex justify-center items-center bg-black rounded-full">
-              <IconContext.Provider value={{ size: "20px", color: "white" }}>
-                <IoIosMenu />
+          <div className="flex items-center gap-8">
+            <div className="sm:flex hidden gap-6">
+              <IconContext.Provider value={{ size: "20px", color: "black" }}>
+                <MdOutlineLocationOn />
+                <RiSearchLine />
               </IconContext.Provider>
+            </div>
+            <div className="flex items-center gap-2">
+              <h4 className="font-medium text-base">MENU</h4>
+              <div className="w-[32px] h-[32px] flex justify-center items-center bg-black rounded-full">
+                <IconContext.Provider value={{ size: "20px", color: "white" }}>
+                  <IoIosMenu />
+                </IconContext.Provider>
+              </div>
             </div>
           </div>
         </div>
