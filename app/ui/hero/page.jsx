de655a21e3cc42e473aa5cc1137aaeadd0b127cd
six-shot <okhuomonajayi54@gmail.com>
2024-02-26 07:54:17 -0800

    first commit

---
+++ app/ui/hero/page.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Navbar from '../navbar/page'
+
+export default function Hero() {
+  return (
+    <div><Navbar/></div>
+  )
+}
---
+++ app/ui/navbar/page.js
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+import { IconContext } from "react-icons";
+import { IoIosMenu } from "react-icons/io";
+export default function Navbar() {
+  return (
+    <header className="bg-white w-full">
+      <div className="max-w-[1440px] mx-auto px-[4%] ">
+        <div className="w-full h-[10vh] border-b-[1px] border-black flex justify-between items-center">
+          <div className="flex items-center gap-2">
+            <h4 className="font-semibold text-[22px]">MERCY</h4>
+            <h4 className="text-[10px] leading-[11px] font-semibold">
+              COSTAL <br />
+              ARCHITECTURE
+            </h4>
+          </div>
+          <div className="flex items-center gap-2">
+            <h4 className="font-medium text-base">MENU</h4>
+            <div className="w-[30px] h-[30px] flex justify-center items-center bg-black rounded-full">
+              <IconContext.Provider value={{size:"18px",color:"white"}}>
+                
+                <IoIosMenu />
+              </IconContext.Provider>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
