d06081e644df7aadd86fcfd3688954d4ac7b6a84
six-shot <okhuomonajayi54@gmail.com>
2024-02-26 09:54:39 -0800

    hero

--- app/ui/hero/page.jsx
+++ app/ui/hero/page.jsx
@@ -34,7 +34,7 @@ export default function Hero() {
                 <div className="sm:hidden flex flex-col">
                   <div className="flex  items-center gap-5">
                     <div className="">
-                      <h4 className="text-right text-[10px] font-medium">
+                      <h4 className="text-right text-[10px] leading-[11px] font-medium">
                         MODERN
                         <br />
                         BEACHFRONT
@@ -63,6 +63,40 @@ export default function Hero() {
                   <h5>All catalog</h5>
                   <div className="w-[9px] h-[9px] bg-white rounded-full"></div>
                 </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mb-[2%] mt-[10%] sm:hidden flex justify-between gap-5 items-center">
+          <div className="sm:hidden flex items-center flex-row">
+            <div className="flex gap-2">
+              <div className="w-[40px] h-[40px] rounded-full border border-black flex justify-center items-center">
+                <div className="w-[9px] h-[9px] bg-black rounded-full"></div>
+              </div>
+              <div className="">
+                <h4 className="text-left text-xs font-medium leading-[11px]">
+                  PARKER
+                  <br />
+                  ALLENTOWN
+                  <br />
+                  NEWMEXICO
+                  <br />
+                  3114
+                </h4>
+              </div>
+            </div>
+          </div>
+          <div className="border border-black    rounded-[50px] w-full  grid grid-cols-12  items-center">
+            <div className="col-span-5 border-r-[1px] border-black h-full">
+              <div className="flex items-center h-full px-3 py-1">
+                <h5 className="text-sm font-semibold">TRUSTED</h5>
+              </div>
+            </div>
+            <div className="col-span-7">
+              <div className="flex flex-col  py-1">
+                <h5 className=" text-sm font-semibold px-3 ">46 Views</h5>
+                <div className="border border-b-black w-full"></div>
+                <h5 className="text-sm font-semibold px-3 ">46 Views</h5>
               </div>
             </div>
           </div>
