bef8ffa5767790363d20a26b84061de06d4af1f5
six-shot <okhuomonajayi54@gmail.com>
2024-02-27 05:24:35 -0800

    wonder

--- app/ui/wonders/page.jsx
+++ app/ui/wonders/page.jsx
@@ -14,7 +14,7 @@ const CircularTextEffect = () => {
   return (
     <div>
       <div className=" h-screen w-full relative">
-        <div className="max-w-[1440px] mx-auto px-[4%] sm:py-[4%]  w-full ">
+        <div className="max-w-[1440px] mx-auto px-[4%]  sm:py-[1%] flex gap-16  sm:gap-0 items-center w-full ">
           <div className="sm:w-[200px] w-[120px]  h-[20vh] flex justify-center items-center">
             <div className=" ">
               <div className="">
@@ -24,6 +24,25 @@ const CircularTextEffect = () => {
               </div>
             </div>
           </div>
+          <hr className="h-[2px] sm:w-[200px] w-full border-[1px] border-black" />
+          <div className="sm:pl-[4%] sm:flex flex-col hidden">
+            <h3 className="text-right text-[23px] font-medium leading-[28px]">
+              DISCOVER THE ARCHITECTUAL
+              <br /> ETCHED ALONG THE SHORELINES,WHERE DESIGNS
+            </h3>
+            <h3 className="text-[23px] font-medium">
+              MEETS THE INFINITE BEAUTY OF THE SEA
+            </h3>
+          </div>
+        </div>
+        <div className=" sm:hidden flex flex-col px-[4%] -mt-4">
+          <h3 className="text-center text-[18px] font-medium leading-[28px]">
+            DISCOVER THE ARCHITECTUAL 
+          </h3>
+          <h3 className="text-[18px] font-medium">
+            ETCHED ALONG THE SHORELINES, <br />
+            WHERE DESIGNS MEETS THE INFINITE BEAUTY OF THE SEA
+          </h3>
         </div>
       </div>
     </div>
